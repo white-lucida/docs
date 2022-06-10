0f5c8f71183f163b49c36d4dda866e975fed2713
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2022-06-10 11:58:56 +0900

    [chore] Discordのメッセージ部分を整形

--- src/components/Discord/index.tsx
+++ src/components/Discord/index.tsx
@@ -4,16 +4,21 @@ import {
 	DiscordMention,
 	DiscordMessage,
 	DiscordMessages,
-	DiscordOptionsContext,
-	DiscordReaction,
-	DiscordReactions,
+  DiscordButton,
+  DiscordButtons,
+  DiscordEmbed,
+  DiscordEmbedField,
+  DiscordEmbedFields,
 } from '@discord-message-components/react'
 
-export  { DiscordMessages as Messages } 
+import '@discord-message-components/react/dist/style.css'
+
+export { DiscordMessages as Messages } 
 
 export const Bot = ({ children }) => {
+  const avatar = "https://cdn.discordapp.com/icons/813577333516402728/a1b8a9ebbf6382f916539a10b5f79315.png"
   return (
-    <DiscordMessage>
+    <DiscordMessage author="White-Lucida" avatar={avatar} bot={true}>
       { children }
     </DiscordMessage>
   )
@@ -26,4 +31,20 @@ export const User = ({ children }) => {
     </DiscordMessage>
   )
 }
+
+
+export { DiscordEmbed as Embed, DiscordEmbedFields as Fields } 
+
+export const Footer = ({ children }) => <span slot="footer">{ children }</span>
+
+export const Buttons = ({ children }) => <DiscordButtons slot="actions">{ children }</DiscordButtons>;
+
+export{ DiscordButton as Button, DiscordMention as Mention, DiscordInteraction as Interaction }
+
+export const Interactions = ({ children }) => <div slot="interactions">{ children }</div>
+
+// export const Fields = ({ children }) => <DiscordEmbedFields slot="fields">{ children }</DiscordEmbedFields>;
+
+
+
 
